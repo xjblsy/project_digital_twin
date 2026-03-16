fab3526e011ddf06c7c27d1b50349793ab3828b1
kami of pku <shouyi_0703@qq.com>
2026-03-16 13:07:53 +0800

    Change default port to 8080 for app execution
    Updated the application to run on port 8080 by default.

--- backend/app.py
+++ backend/app.py
@@ -109,5 +109,6 @@ def batch_predict():
         }), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    import os
+    port = int(os.environ.get('PORT', 8080))  # Zeabur 默认端口 8080
     app.run(host='0.0.0.0', port=port, debug=False)
